b990c27d179134f4dc64f1a454c1256c1b8d074a
Luis Moreno <luis.moreno230896@gmail.com>
2020-06-08 06:49:58 +0000

    Trabajando con Sinatra

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require './config'
+require './lib/anfitrion'
 
 get '/' do
-    'hola mundo'
+    erb :hola
+end
+
+post '/saludo' do
+    anfitrion = Anfitrion.new params['idioma']
+    @mensaje = anfitrion.saluda params['nombre']
+    erb :saludo
+
+    
 end
